9bd5069b892a4867a7bee588ab41afcb14f79220
minirachel <rachel.a.levinson@gmail.com>
2021-06-09 13:49:28 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,19 +7,21 @@ class SongsController < ApplicationController
 
     get '/songs/new' do
         @artists = Artist.all
+        @genres = Genre.all
         erb :'songs/new'
     end
 
     post '/songs' do
         @song = Song.create(params[:song])
+        @song.genre_ids = params[:genres]
 
         if !params["artist"]["name"].empty?
             @song.artist = Artist.create(name: params["artist"]["name"])
+        else
+            @song.artist = Artist.find(params["song"]["artist_id"])
         end
 
         @song.save
-
-        binding.pry
 
         redirect to "/songs/#{@song.slug}"
     end
