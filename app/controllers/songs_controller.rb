93b8b12b977b9bb975b89c56d2a9e9197e3e6ea7
minirachel <rachel.a.levinson@gmail.com>
2021-06-09 14:31:11 -0400

    commit tree 81a8908323ba0e0d1618f0f285926474bd0c32b8

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -38,7 +38,29 @@ class SongsController < ApplicationController
     end
 
     get '/songs/:slug/edit' do
+        @song = Song.find_by_slug(params[:slug])
+        @artists = Artist.all
+        @genres = Genre.all
+
         erb :'songs/edit'
+    end
+
+    patch '/songs/:slug' do
+        @song = Song.find_by_slug(params[:slug])
+        @song.update(params[:song])
+        @song.genre_ids = params[:genres]
+
+        if !params["artist"]["name"].empty?
+            @song.artist = Artist.find_or_create_by(name: params["artist"]["name"])
+        else
+            @song.artist = Artist.find(params["song"]["artist_id"])
+        end
+
+        @song.save
+
+        flash[:message] = "Successfully updated song."
+        
+        redirect to "/songs/#{@song.slug}"
     end
 
 end
